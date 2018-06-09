3c34682f18f7c9b8c28c4c693f0f7a58bc75e1c1
Nicolas Aguerre <aguerre31@gmail.com>
2018-06-09 16:45:25 -0300

    Agregado el constructor de Gunpla

--- clases.py
+++ clases.py
@@ -1,1 +1,12 @@
-sdasd
+class Gunpla:
+	'''
+	Representa un Gunpla. Un Gunpla esta compuesto de un Esqueleto, un conjunto de partes y un conjunto de armas.
+	'''
+
+	def __init__(self,esqueleto):
+		'''
+		Recibe un esqueleto e inicializa el gunpla con ese esqueleto.
+		'''
+		self.esqueleto = esqueleto 
+		self.partes = []
+		self.armas = []
