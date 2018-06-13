85bc7f70fdb9e30ef05e246b125a19b6324709e1
Nicolas Aguerre <aguerre31@gmail.com>
2018-06-13 14:43:15 -0300

    Agregado el metodo generar_partes a main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from clases import *
+from Pila import *
 
 CANTIDAD_ESQUELETOS = 3
+CANTIDAD_PARTES = 50
 
 def generar_esqueletos():
 	'''
@@ -8,7 +10,24 @@ def generar_esqueletos():
 	'''
 	esqueletos = []
 
-	for i in range CANTIDAD_ESQUELETOS:
+	for i in range(CANTIDAD_ESQUELETOS):
 		esqueletos.append(Esqueleto())
 
 	return esqueletos
+
+def generar_partes():
+	'''
+	Devuelve una pila con CANTIDAD_PARTES de partes generadas de forma aleatoria, con sus respectivas armas equipadas.
+	'''
+	partes = Pila()
+
+	for _ in range(CANTIDAD_PARTES):
+		parte = Parte()
+
+		#Se adosan las armas correspondientes a la parte segun sus slots disponibles.
+		for _ in range(parte.get_cantidad_slots()):
+			parte.attach_arma(Arma())
+
+		partes.apilar(parte)
+
+	return partes
