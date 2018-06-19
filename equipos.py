ba2f5009db67799b5a3a07d81e7515a6ec86d693
jpdicomo <jpdicomo@live.com>
2018-06-19 18:31:03 -0300

    Eliminada la funcion elegir_equipos y creada la clase Equipo con el metodo get_pilotos_vivos

--- equipos.py
+++ equipos.py
@@ -1,17 +1,16 @@
-import random
+class Equipo:
+	"""Representa un equipo de pilotos"""
 
-def generar_equipos(lista_pilotos):
-	"""Recibe todos los pilotos y los divide en equipos de PILOTOS_POR_EQUIPO,devuelve una lista con todos los equipos"""
+	def __init__(pilotos):
+		"""Recibe una lista con los objetos Piloto pertenecientes al equipo"""
 
-	equipos = {"equipo {}".format(i):[] for i in range(lista_pilotos)}
-	
-	random.shuffle(lista_pilotos)
-	
-	indice=0
+		self.pilotos = pilotos
+		self.pilotos_vivos = []
+	def get_pilotos_vivos():
 
-	for piloto in lista_pilotos:
-		equipos["equipo {}".format(indice)].append(piloto)
-		if len(equipos["equipo {}".format(indice)])==PILOTOS_POR_EQUIPO:
-			indice+=1
+		for _,piloto in self.pilotos:
+			if piloto.get_gunpla().get_energia_restante()<=0:
+				continue
+			self.pilotos_vivos.append()
+		return self.pilotos_vivos
 
-	return equipos
