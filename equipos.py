4865744a23c06ea736de08f8364e13a8a97ed734
Nicolas Aguerre <aguerre31@gmail.com>
2018-06-20 03:16:25 -0300

    cambiada la logica de algunos metodos

--- equipos.py
+++ equipos.py
@@ -1,47 +1,65 @@
 NOMBRES_EQUIPO = ("Abomyzation","Ansatsu","Banshee","Bloodbane","Collapse","Cyberstein","Darkness","Defussion","Extermination","Flamecaster")
 
-
 class Equipo:
 	'''
-	Representa un equipo de pilotos que tiene un nombre 
-	y puede determinar que pilotos siguen vivos
+	Representa un equipo de participantes que tiene un nombre 
+	y puede determinar que participantes siguen vivos
 	'''
 
-	def __init__(self,pareja):
-		'''Recibe una lista con los objetos Piloto pertenecientes al equipo'''
+	def __init__(self):
+		'''Recibe una lista con los objetos participante pertenecientes al equipo'''
 
-		self.pilotos = pareja
-		self.pilotos_vivos = []
+		self.participantes = []
 		self.nombre = ""
 		
-	def get_pilotos_vivos(self):
-		'''Devuelve una lista con los pilotos que siguen vivos'''
-		pilotos_vivos = []
+	def get_participantes_vivos(self):
+		'''Devuelve una lista con los participantes que siguen vivos'''
+		participantes_vivos = []
 
-		for piloto in self.pilotos:
-			if piloto.get_gunpla().get_energia_restante()<=0:
+		for participante in self.participantes:
+
+			if participante.get_piloto().get_gunpla().get_energia_restante()<=0:
 				continue
-			pilotos_vivos.append(piloto)
 
-		return pilotos_vivos
+			participantes_vivos.append(participante)
+		return participantes_vivos
+
+	def get_participantes(self):
+		'''
+		Devuelve una lista con todos los participantes pertenecientes al equipo.
+		'''
+		return self.participantes
+
 	def asignar_nombre(self,nombres_ocupados):
 		'''Selecciona el nombre del equipo'''
 		nombres_disponibles = [nombre for nombre in NOMBRES_EQUIPO if nombre not in nombres_ocupados]
 		self.nombre=random.choice(NOMBRES_EQUIPO)
 		return self.nombre
-		
+
 	def get_nombre(self):
 		'''Devuelve el nombre del equipo'''
 		return self.nombre
+
+	def agregar_participante(self,participante):
+		'''
+		Recibe un participante y lo agrega al equipo.
+		'''
+		self.participantes.append(participante)
 
 class Participante:
 	'''Representa un participante de un equipo, puede saber a que equipo pertenece'''
 
 	def __init__(self,piloto,equipo):
-		'''Recibe un objeto piloto y un objeto equipo'''
-		self.piloto=piloto
-		self.equipo=equipo
+		'''Recibe un objeto participante y un objeto equipo'''
+		self.piloto = piloto
+		self.equipo = equipo
 
+	def get_piloto(self):
+		'''
+		Devuelve el piloto al cual representa el participante.
+		'''
+		return self.piloto
+		
 	def get_equipo(self):
 		'''Devuelve el equipo del participante'''
 
