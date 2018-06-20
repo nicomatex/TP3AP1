2144d13f489f2b3934cd68f47b3e72ecc2e69f3d
Nicolas Aguerre <aguerre31@gmail.com>
2018-06-20 01:37:37 -0300

    cambiada la logica de asignar_nombre

--- equipos.py
+++ equipos.py
@@ -1,50 +1,48 @@
-import random
-
 NOMBRES_EQUIPO = ("Abomyzation","Ansatsu","Banshee","Bloodbane","Collapse","Cyberstein","Darkness","Defussion","Extermination","Flamecaster")
 
+
 class Equipo:
-	"""
+	'''
 	Representa un equipo de pilotos que tiene un nombre 
 	y puede determinar que pilotos siguen vivos
-	"""
+	'''
 
 	def __init__(self,pareja):
-		"""Recibe una lista con los objetos Piloto pertenecientes al equipo"""
+		'''Recibe una lista con los objetos Piloto pertenecientes al equipo'''
 
 		self.pilotos = pareja
 		self.pilotos_vivos = []
-		self.nombre = random.choice(NOMBRES_EQUIPO)
+		self.nombre = ""
+		
 	def get_pilotos_vivos(self):
-		"""Devuelve una lista con los pilotos que siguen vivos"""
+		'''Devuelve una lista con los pilotos que siguen vivos'''
+		pilotos_vivos = []
+
 		for piloto in self.pilotos:
 			if piloto.get_gunpla().get_energia_restante()<=0:
 				continue
-			self.pilotos_vivos.append()
-		return self.pilotos_vivos
+			pilotos_vivos.append(piloto)
 
-	def seleccionar_nombre(self):
-		"""Selecciona el nombre del equipo"""
+		return pilotos_vivos
+	def asignar_nombre(self,nombres_ocupados):
+		'''Selecciona el nombre del equipo'''
+		nombres_disponibles = [nombre for nombre in NOMBRES_EQUIPO if nombre not in nombres_ocupados]
 		self.nombre=random.choice(NOMBRES_EQUIPO)
-
+		return self.nombre
+		
 	def get_nombre(self):
-		"""Devuelve el nombre del equipo"""
+		'''Devuelve el nombre del equipo'''
 		return self.nombre
 
 class Participante:
-	"""Representa un participante de un equipo, puede saber a que equipo pertenece"""
+	'''Representa un participante de un equipo, puede saber a que equipo pertenece'''
 
 	def __init__(self,piloto,equipo):
-		"""Recibe un objeto piloto y un objeto equipo"""
+		'''Recibe un objeto piloto y un objeto equipo'''
 		self.piloto=piloto
 		self.equipo=equipo
 
 	def get_equipo(self):
-		"""Devuelve el equipo del participante"""
+		'''Devuelve el equipo del participante'''
 
 		return self.equipo
-
-	def get_piloto(self):
-		"""Devuelve el piloto participante"""
-
-		return self.piloto
-		
